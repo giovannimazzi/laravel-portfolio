ef7324a11e85f9576a6f9dfcddca2d344e359678
giovannimazzi <gioelor@libero.it>
2026-05-28 03:44:17 +0200

    add projects-show view

---
+++ resources/views/crud/projects-show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="card shadow-sm">
+        
+        <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+            <h3 class="mb-0">Project Details</h3>
+            <a href="{{route("projects.index")}}" class="btn btn-outline-dark">
+                <i class="bi bi-chevron-double-left"></i>
+                Back to List
+            </a>
+        </div>
+        <div class="card-body">
+            <h5>Name:</h5>
+            <p class="fw-bold"><big>{{$project->name}}</big></p>
+            <hr/>
+            <h5>Customer:</h5>
+            <p>{{$project->customer}}</p>
+            <hr/>
+            <h5>Description:</h5>
+            <p>{{$project->description}}</p>
+            <hr/>
+            <h5>Start Date:</h5>
+            <p>{{$project->start_date}}</p>
+            <hr/>
+            <h5>End Date:</h5>
+            <p>{{$project->end_date}}</p>
+            <hr/>
+        </div>
+    </div>
+
+@endsection
