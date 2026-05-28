4a2e1655f6a1ae3207c7e532b54b33bf1a84f68a
giovannimazzi <gioelor@libero.it>
2026-05-28 03:06:35 +0200

    add ProjectController->index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -12,7 +12,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::all();
+        return view('crud.projects-index', compact('projects'));
     }
 
     /**
