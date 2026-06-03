837c3fe9e562b48a95abc2143f1add3bbbc9ae27
giovannimazzi <gioelor@libero.it>
2026-06-03 17:05:50 +0200

    add ManyToMany relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     public function type(){
-        return $this->belongsTo(Type::class); //nel model dipendente
+        return $this->belongsTo(Type::class); //nel model dipendente [1 tipo per progetto]
+    }
+
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
     }
 }
